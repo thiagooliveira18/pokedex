cb3757e7485eeb17ebfff5077ee8e9a250fe1e96
Thiago Oliveira <62665295+thiagooliveira18@users.noreply.github.com>
2024-08-23 12:09:59 -0300

    feat: testando abertura de pr (#1)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import styles from './page.module.css';
 import Header from '@/components/Header';
 import Body from '@/components/Body';
 
-
+// Comentário teste para pull request
 
 export default function Home() {
   return (
